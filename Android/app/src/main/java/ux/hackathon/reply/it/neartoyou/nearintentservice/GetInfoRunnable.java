ef94ed9506e12216f259937f226df2bcef6b2d1d
Claudio Rauso <clarauso@gmail.com>
2016-03-19 19:40:51 +0100

    runnable with gps position

--- Android/app/src/main/java/ux/hackathon/reply/it/neartoyou/nearintentservice/GetInfoRunnable.java
+++ Android/app/src/main/java/ux/hackathon/reply/it/neartoyou/nearintentservice/GetInfoRunnable.java
@@ -1,6 +1,12 @@
 package ux.hackathon.reply.it.neartoyou.nearintentservice;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.util.Log;
+
+import static android.location.LocationManager.GPS_PROVIDER;
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 /**
  * Created by c.rauso on 19/03/2016.
@@ -9,5 +15,13 @@ public class GetInfoRunnable implements Runnable {
     @Override
     public void run() {
         Log.d("NEAR TO YOU RUNNABLE", "This is a Runnable");
+        String locationProvider = GPS_PROVIDER;
+        LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        try {
+            Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+            Log.d("NEAR TO YOU RUNNABLE", (Double.toString(lastKnownLocation.getLatitude()) + " " + Double.toString(lastKnownLocation.getLongitude())));
+        } catch (SecurityException e) {
+            Log.e("NEAR TO YOU RUNNABLE", e.getMessage());
+        }
     }
 }
