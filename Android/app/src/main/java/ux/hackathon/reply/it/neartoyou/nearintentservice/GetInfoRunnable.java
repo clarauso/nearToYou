e6a65db4e2ca7512883bbd62ec95bf9f64b48323
Claudio Rauso <clarauso@gmail.com>
2016-03-20 00:45:31 +0100

    added notification action

--- Android/app/src/main/java/ux/hackathon/reply/it/neartoyou/nearintentservice/GetInfoRunnable.java
+++ Android/app/src/main/java/ux/hackathon/reply/it/neartoyou/nearintentservice/GetInfoRunnable.java
@@ -1,13 +1,17 @@
 package ux.hackathon.reply.it.neartoyou.nearintentservice;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
 import ux.hackathon.reply.it.neartoyou.R;
+import ux.hackathon.reply.it.neartoyou.RecommendationsActivity;
 
 import static android.location.LocationManager.GPS_PROVIDER;
 import static com.facebook.FacebookSdk.getApplicationContext;
@@ -16,6 +20,9 @@ import static com.facebook.FacebookSdk.getApplicationContext;
  * Created by c.rauso on 19/03/2016.
  */
 public class GetInfoRunnable implements Runnable {
+
+    Intent notificationIntent = new Intent(getApplicationContext(), RecommendationsActivity.class);
+
     @Override
     public void run() {
         Log.d("NEAR TO YOU RUNNABLE", "This is a Runnable");
@@ -31,16 +38,25 @@ public class GetInfoRunnable implements Runnable {
     }
 
     private void nearNotification() {
+        // sets the Intent to open a RecommendationsActivity
         Log.d("NEAR TO YOU RUNNABLE", "Creazione di una nuova notifica");
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
+        stackBuilder.addParentStack(RecommendationsActivity.class);
+        stackBuilder.addNextIntent(notificationIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // builds the notification
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext())
                 .setSmallIcon(R.drawable.globe)
                 .setContentTitle("NearToYou")
-                .setContentText("I've found new interesting places for you!");
+                .setContentText("I've found new interesting places for you!")
+                .setContentIntent(resultPendingIntent);
 
+        // add the new notification to the NotificationManager
         int mNotificationId = 001;
-
         NotificationManager mNotifyMgr = (NotificationManager)getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
 
+        // show the notification
         mNotifyMgr.notify(mNotificationId, mBuilder.build());
     }
 }
