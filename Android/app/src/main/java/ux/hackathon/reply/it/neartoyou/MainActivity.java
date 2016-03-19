f9fd4cfb38e2baffbb86a96b4c5cef4fe9c26054
Claudio Rauso <clarauso@gmail.com>
2016-03-19 14:04:43 +0100

    added Facebook SDK inizialization

--- Android/app/src/main/java/ux/hackathon/reply/it/neartoyou/MainActivity.java
+++ Android/app/src/main/java/ux/hackathon/reply/it/neartoyou/MainActivity.java
@@ -1,14 +1,18 @@
 package ux.hackathon.reply.it.neartoyou;
 
-import com.facebook.FacebookSdk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.facebook.FacebookSdk;
+
+import static com.facebook.FacebookSdk.sdkInitialize;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this);
         setContentView(R.layout.activity_main);
     }
 }
