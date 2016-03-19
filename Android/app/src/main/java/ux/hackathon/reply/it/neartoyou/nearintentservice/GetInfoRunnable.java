440ab5dbfddacfea1f087b589824749602a76726
Claudio Rauso <clarauso@gmail.com>
2016-03-19 17:14:08 +0100

    scheduled task step 1

---
+++ Android/app/src/main/java/ux/hackathon/reply/it/neartoyou/nearintentservice/GetInfoRunnable.java
@@ -0,0 +1,13 @@
+package ux.hackathon.reply.it.neartoyou.nearintentservice;
+
+import android.util.Log;
+
+/**
+ * Created by c.rauso on 19/03/2016.
+ */
+public class GetInfoRunnable implements Runnable {
+    @Override
+    public void run() {
+        Log.d("NEAR TO YOU RUNNABLE", "This is a Runnable");
+    }
+}
---
+++ Android/app/src/main/java/ux/hackathon/reply/it/neartoyou/nearintentservice/NearToYouIntentService.java
@@ -0,0 +1,37 @@
+package ux.hackathon.reply.it.neartoyou.nearintentservice;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by c.rauso on 19/03/2016.
+ */
+public class NearToYouIntentService  extends IntentService {
+
+    private ScheduledThreadPoolExecutor executor;
+
+    public NearToYouIntentService() {
+        super("NearToYouIntentService");
+    }
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public NearToYouIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d("NearToYouIntentService", "NearToYouIntentService start scheduling task");
+        executor = new ScheduledThreadPoolExecutor(0);
+        executor.scheduleAtFixedRate(new GetInfoRunnable(), 0, 3, TimeUnit.MINUTES);
+        Log.d("NearToYouIntentService", "Task scheduled");
+    }
+}
