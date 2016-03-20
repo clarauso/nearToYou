eefed6932fac1c4b0e99fb59349aea4f5e635938
Claudio Rauso <clarauso@gmail.com>
2016-03-20 12:54:29 +0100

    demo version

--- Android/app/src/main/java/ux/hackathon/reply/it/neartoyou/MainActivity.java
+++ Android/app/src/main/java/ux/hackathon/reply/it/neartoyou/MainActivity.java
@@ -7,32 +7,45 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.facebook.FacebookSdk;
 
+import ux.hackathon.reply.it.neartoyou.nearintentservice.NearToYouIntentService;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 import static com.facebook.FacebookSdk.sdkInitialize;
 
 public class MainActivity extends AppCompatActivity {
     CallbackManager callbackManager;
+    Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
         callbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton;
+        final LoginButton loginButton;
         loginButton = (LoginButton)findViewById(R.id.login_button);
         loginButton.setReadPermissions("user_friends");
-
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                System.out.println(loginResult.getAccessToken().getUserId());
+                Log.d("LOGIN SUCCESS CALLBACK", loginResult.getAccessToken().getUserId());
+                Log.d("LOGIN SUCCESS CALLBACK", "Invoco la partenza dell'intent");
+                startIntentService();
+                Toast.makeText(getApplicationContext(), "I'll be with you!", Toast.LENGTH_LONG).show();
+                finish();
             }
 
             @Override
@@ -42,10 +55,22 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onError(FacebookException exception) {
-                // App code
+                Log.e(this.toString(), exception.getMessage());
             }
         });
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void startIntentService() {
+        // fa partire l'intent per la ripetizione periodica della chiamata
+        Log.d("LOGIN SUCCESS CALLBACK", "Entro in startIntentService");
+        serviceIntent = new Intent(this, NearToYouIntentService.class);
+        this.startService(serviceIntent);
+    }
 
 }
--- Android/app/src/main/java/ux/hackathon/reply/it/neartoyou/recommendations/RecommendationData.java
+++ Android/app/src/main/java/ux/hackathon/reply/it/neartoyou/recommendations/RecommendationData.java
@@ -15,22 +15,25 @@ public class RecommendationData {
     public static HashMap<String, List<Recommendation>> getData() {
         HashMap<String, List<Recommendation>> expandableListDetail = new HashMap<String, List<Recommendation>>();
 
+        // restaurants
         List<Recommendation> restaurants = new ArrayList<Recommendation>();
-
         Recommendation toAdd = new Recommendation();
         toAdd.setName("Joia");
         toAdd.setDescription("Rinomato ristorante italiano noto per le specialità di mare.");
-        toAdd.setLatitude(45.650208);
-        toAdd.setLongitude(13.767849);
-        restaurants.add(toAdd);
+        toAdd.setLatitude(45.649283);
+        toAdd.setLongitude(13.766630);
+        if(Math.random() < 0.5) {
+            restaurants.add(toAdd);
+        }
 
         toAdd = new Recommendation();
         toAdd.setName("Sushi bar");
         toAdd.setDescription("Ottimo ristorante giapponese con cucina a vista.");
-        toAdd.setLatitude(45.650208);
-        toAdd.setLongitude(13.767849);
+        toAdd.setLatitude(45.649486);
+        toAdd.setLongitude(13.767011);
         restaurants.add(toAdd);
 
+        // art
         List<Recommendation> monuments = new ArrayList<Recommendation>();
         toAdd = new Recommendation();
         toAdd.setName("Piazza Unità d'Italia");
@@ -41,14 +44,69 @@ public class RecommendationData {
         monuments.add(toAdd);
 
         toAdd = new Recommendation();
-        toAdd.setName("Sala degli Incanti");
-        toAdd.setDescription("Ex mercato del pesce.");
-        toAdd.setLatitude(45.650208);
-        toAdd.setLongitude(13.767849);
+        toAdd.setName("Salone degli Incanti");
+        toAdd.setDescription("L'ex mercato del pesce, riqualificato per accogliere mostre ed esposizioni.");
+        toAdd.setLatitude(45.647868);
+        toAdd.setLongitude(13.762226);
         monuments.add(toAdd);
 
+        // leisure
+        List<Recommendation> leisure = new ArrayList<Recommendation>();
+        toAdd = new Recommendation();
+        toAdd.setName("Night Club Mexico");
+        toAdd.setDescription("Night club storico di Trieste, da provare almeno una volta.");
+        toAdd.setLatitude(45.652148);
+        toAdd.setLongitude(13.774474);
+        leisure.add(toAdd);
+
+        toAdd = new Recommendation();
+        toAdd.setName("Streaming Discoteca");
+        toAdd.setDescription("Discoteca dalla frequentazione molto variegata, appuntamento fisso del sabato triestino.");
+        toAdd.setLatitude(45.656669);
+        toAdd.setLongitude(13.804542);
+        if(Math.random() < 0.5) {
+            leisure.add(toAdd);
+        }
+
+        // museums
+        List<Recommendation> museums = new ArrayList<Recommendation>();
+        toAdd = new Recommendation();
+        toAdd.setName("Museo Ferroviario");
+        toAdd.setDescription("Il Museo ferroviario di Trieste Campo Marzio è un museo triestino dedicato alla storia delle ferrovie del Friuli e della Venezia Giulia, e i relativi mezzi di locomozione, personale, sistemi di manutenzione e gestione.");
+        toAdd.setLatitude(45.646324);
+        toAdd.setLongitude(13.755310);
+        museums.add(toAdd);
+
+        toAdd = new Recommendation();
+        toAdd.setName("Civico museo sartorio");
+        toAdd.setDescription("Il civico museo Sartorio si trova a Trieste. Custodisce arredi ed oggetti d'epoca, ceramiche, biblioteca, raccolte di quadri, disegni di Giambattista Tiepolo ed una gipsoteca-gliptoteca.");
+        toAdd.setLatitude(45.646106);
+        toAdd.setLongitude(13.763013);
+        if(Math.random() < 0.5) {
+            museums.add(toAdd);
+        }
+        // shopping
+        List<Recommendation> shopping = new ArrayList<Recommendation>();
+        toAdd = new Recommendation();
+        toAdd.setName("Atelier Aimée Trieste");
+        toAdd.setDescription("Storico negozio di abiti da sposa.");
+        toAdd.setLatitude(45.647986);
+        toAdd.setLongitude(13.764665);
+        shopping.add(toAdd);
+
+        toAdd = new Recommendation();
+        toAdd.setName("Falconeri Maglieria e Abbigliamento in Cashmere");
+        toAdd.setDescription("Negozio di abbigliamento, offre capi ricercati in un ambiente elegante.");
+        toAdd.setLatitude(45.650401);
+        toAdd.setLongitude(13.771431);
+        shopping.add(toAdd);
+
+        expandableListDetail.put("ART", monuments);
+        expandableListDetail.put("LEISURE", leisure);
+        expandableListDetail.put("MUSEUM", museums);
         expandableListDetail.put("RESTAURANTS", restaurants);
-        expandableListDetail.put("MONUMENTS", monuments);
+        expandableListDetail.put("SHOPPING", shopping);
+
         return expandableListDetail;
     }
 
